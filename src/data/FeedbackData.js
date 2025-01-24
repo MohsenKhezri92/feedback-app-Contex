589410d16db95f7f488367862361c29510ef5d9f
MohsenKhezri92 <mohsenkhezri92@gmail.com>
2025-01-24 10:54:58 -0800

    managing global state

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'lorem ipsom '
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'lorem ipsom '
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'lorem ipsom '
+    },
+]
+
+export default FeedbackData
