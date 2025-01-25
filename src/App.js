8c3d97beaf793d26438645e8da67f874303da910
Mohsen Khezri <mohsenkhezri92@gmail.com>
2025-01-25 15:53:23 +0330

    Form Input & State

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,6 +19,7 @@ function App() {
     <>
       <Header text="Feedback UI" />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList
           feedback={feedback}
