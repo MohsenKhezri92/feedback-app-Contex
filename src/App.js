03ab4372c5e01503fee609c43c454a5b5a762c72
Mohsen Khezri <mohsenkhezri92@gmail.com>
2025-01-30 12:04:03 +0330

    installing uuid and Add Feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid' 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
@@ -8,6 +9,11 @@ import FeedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deletefeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')){
@@ -19,7 +25,7 @@ function App() {
     <>
       <Header text="Feedback UI" />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handeAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList
           feedback={feedback}
